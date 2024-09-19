58ff32becf1a37da6ebcf4c9973fa3a062dcb1b7
liuxspro <liuxspro@gmail.com>
2024-09-19 11:22:35 +0800

    Chore: 地质云代理

---
+++ src/libs/mod.rs
@@ -0,0 +1,19 @@
+pub mod geocloud;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    // use std::fs::write;
+
+    #[tokio::test]
+    async fn get_geocloud_tile() {
+        let result =
+            geocloud::get_geocloud_tile(4, 24, 5, "qg250w_20210416_ZAZSeOGX".to_string()).await;
+
+        assert!(result.is_ok(), "get_geocloud_tile should return Ok");
+        // 如果结果为 Ok，进一步验证返回的 body 是否符合预期
+        if let Ok(body) = result {
+            assert!(body.len() == 15301, "Body length should be 15301");
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,46 @@
-use reqwest::Error;
-use std::collections::HashMap;
-use std::fs::write;
+#[macro_use]
+extern crate rocket;
 
-#[tokio::main]
-async fn main() {
-    match get_geocloud_tile(4, 24, 5, "qg250w_20210416_ZAZSeOGX".to_string()).await {
-        Ok(body) => {
-            println!("Response body length: {}", body.len());
-            println!("body preview: {:?}", &body[..10]);
-            write("test.png", body).expect("Filed to write file");
-        }
-        Err(e) => {
-            println!("error: {}", e);
-        }
+use rocket::response::content::RawXml;
+use rocket::{http::Status, response::status};
+use std::fs;
+
+mod libs;
+use libs::geocloud::get_geocloud_tile;
+use rocket::http::ContentType;
+
+#[derive(FromForm)]
+struct GeoCloudQuery {
+    layer: String,
+}
+
+#[get("/getTile/geocloud/<z>/<x>/<y>?<query..>")]
+async fn get_geocloud(
+    z: u32,
+    x: u32,
+    y: u32,
+    query: GeoCloudQuery,
+) -> Result<(ContentType, Vec<u8>), status::Custom<String>> {
+    match get_geocloud_tile(z, x, y, query.layer).await {
+        Ok(body) => Ok((ContentType::PNG, body)),
+        Err(e) => Err(status::Custom(
+            Status::InternalServerError,
+            format!("Error is: {}", e),
+        )),
     }
 }
 
-fn create_parms(z: u32, x: u32, y: u32, layer: String) -> HashMap<&'static str, String> {
-    let mut params = HashMap::new();
-    let tk:String = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJnZW9jbG91ZCIsImV4cCI6MTcyNzI1NTM3NH0.wdDEJ8-n8ylzF8g2ludHRWVX9TTqLO7u_nRbItX-v7M".to_string();
-
-    params.insert("tk", tk);
-    params.insert("Width", "256".to_string());
-    params.insert("Height", "256".to_string());
-    params.insert("layer", layer);
-    params.insert("style", "default".to_string());
-    // tilematrixset 参数似乎不需要
-    // params.insert("tilematrixset", format!("EPSG%3A4326_{}_028mm_GB", layer.clone));
-    params.insert("Service", "WMTS".to_string());
-    params.insert("Request", "GetTile".to_string());
-    params.insert("Version", "1.0.0".to_string());
-    params.insert("Format", "image%2Fpng".to_string());
-    params.insert("TileMatrix", z.to_string());
-    params.insert("TileCol", x.to_string());
-    params.insert("TileRow", y.to_string());
-
-    return params;
+#[get("/WMTS/geocloud")]
+fn get_geocloud_wmts() -> RawXml<String> {
+    let file_content = fs::read_to_string("geocloud.xml").expect("Unable to read file");
+    // 返回 XML 内容
+    RawXml(file_content)
 }
 
-async fn get_geocloud_tile(z: u32, x: u32, y: u32, layer: String) -> Result<Vec<u8>, Error> {
-    let url = format!(
-        "https://igss.cgs.gov.cn:6160/igs/rest/ogc/{}/WMTSServer",
-        layer
-    );
-
-    let params = create_parms(z, x, y, layer);
-
-    let client = reqwest::Client::builder().build()?;
-    // 发送 GET 请求
-    let response = client.get(url).query(&params).send().await?;
-    let body = response.bytes().await?;
-    Ok(body.to_vec())
+#[launch]
+fn rocket() -> _ {
+    println!("Tiny Tile Proxy");
+    println!("");
+    println!("使用: 将 http://127.0.0.1:8000/WMTS/geocloud 添加为 QGIS WMTS 连接");
+    rocket::build().mount("/", routes![get_geocloud, get_geocloud_wmts])
 }
